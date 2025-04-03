6df893b08695930182bda914dd7aff89b095a9e4
Timo Lechner <timo.lechner@d-fine.com>
2025-04-03 17:54:17 +0200

    Add plot

--- R/data_analysis.R
+++ R/data_analysis.R
@@ -75,3 +75,42 @@ ggcorrplot(cor_matrix,
            lab_size = 3,  # Adjust label size
            colors = c("#BB4444", "#FFFFFF", "#4477AA"),  # Red-White-Blue gradient
            ggtheme = theme_minimal())
+
+data_home_goals <- data %>%
+  group_by(HomeTeam) %>%
+  summarize(home_goals = sum(FTHG),
+            home_games = n())
+
+data_away_goals <- data %>%
+  group_by(AwayTeam) %>%
+  summarize(away_goals = sum(FTAG),
+            away_games = n())
+
+data_join_goals <- data_home_goals %>%
+  left_join(data_away_goals, by = join_by(HomeTeam == AwayTeam)) %>%
+  rename(Team = HomeTeam) %>%
+  mutate(goal_ratio = home_goals / (home_goals + away_goals),
+         total_games = home_games + away_games,
+         deviation = goal_ratio - mean(goal_ratio))
+
+ggplot(data = data_join_goals, aes(x = reorder(Team, -goal_ratio), y = goal_ratio, fill = Team)) +
+  geom_col(show.legend = FALSE, width = 0.7, color = "black") +  # Adds black borders for contrast
+  scale_fill_manual(values = team_colors) +
+  scale_y_continuous(breaks = seq(0.4, 0.7, by = 0.1), labels = scales::percent_format(accuracy = 1)) +
+  labs(x = NULL, y = "Percentage of Home Goals to Total Goals") +
+  theme_minimal(base_size = 14) +
+  coord_cartesian(ylim = c(0.4, max(data_join_goals$goal_ratio))) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, face = "bold")
+  )
+
+# Create the plot
+ggplot(data_join_goals, aes(x = total_games, y = deviation, label = Team)) + 
+  geom_point(aes(color = Team), size = 3) +  # Color the points
+  geom_text_repel(vjust = 1.5, hjust = 0.5, size = 3, max.overlaps = 10) +  # Keep text in default color
+  geom_hline(yintercept = 0, linewidth = 1) +
+  geom_vline(xintercept = 0, linewidth = 1) + 
+  theme_minimal() +
+  scale_color_manual(values = team_colors) +
+  theme(legend.position="none")
+
